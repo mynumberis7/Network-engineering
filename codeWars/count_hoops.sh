2a40d2abba286ca562c1a8fc4af184b41868894d
lab906623 <81423807+mynumberis7@users.noreply.github.com>
2026-04-06 10:08:04 +0500

    Resolved count_hoops.sh
    Task resolved

--- codeWars/count_hoops.sh
+++ codeWars/count_hoops.sh
@@ -1,1 +1,13 @@
+#!/bin/bash
 
+n=$1 #first argument
+
+if [ $n -ge 10 ]
+then
+    echo "Great, now move on to tricks"
+elif [$n -eq 10 ]
+then
+    echo "Great, now move on to tricks"
+else
+    echo "Keep at it until you get it"
+fi
